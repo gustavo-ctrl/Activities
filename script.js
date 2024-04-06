f541e091861afa74801f68a8331773c20ccc0352
RianFe.dev <158506928+RianzDev@users.noreply.github.com>
2024-04-06 20:52:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<nav>
+	<ul>
+		<li><a href="index.html">Início</a></li>
+		<li><a href="produtos.html">Produtos</a></li>
+		<li><a href="carrinho.html">Carrinho</a></li>
+		<li><a href="contato.html">Contato</a></li>
+		<br>
+	</ul>
+</nav>
